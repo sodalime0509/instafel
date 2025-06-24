b516becf543248c8bc7a0399411f96e69af5a7b4
mami <70213359+mamiiblt@users.noreply.github.com>
2025-06-24 09:40:08 +0000

    feat: Use toggle based theme switcher button

--- website/src/components/ThemeSwitcher.tsx
+++ website/src/components/ThemeSwitcher.tsx
@@ -3,18 +3,12 @@
 import { useState, useEffect, Suspense } from "react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import { Monitor, Moon, Sun } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Moon, Sun } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
   const { t } = useTranslation("common");
 
   useEffect(() => {
@@ -30,27 +24,20 @@ export default function ThemeSwitcher() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={"ghost"} size="icon" className="relative">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-4 w-4" />
-          <span>{t("navbar.2")}</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>{t("navbar.3")}</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Monitor className="mr-2 h-4 w-4" />
-          <span>{t("navbar.4")}</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant={"ghost"}
+      size="icon"
+      className="relative"
+      onClick={() => {
+        if (theme == "light") {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      }}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
   );
 }
